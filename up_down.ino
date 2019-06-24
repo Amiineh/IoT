2f29f3e120f724a9aa546fb56eeadaeed12880d1
amineh.ahm <amineh.ahm@gmail.com>
2019-06-24 13:40:04 +0430

    rainbow colors

--- up_down.ino
+++ up_down.ino
@@ -9,20 +9,14 @@ const int BRIGHTNESS = 60;
 const int HEIGHT = 12;
 const int WIDTH = 10;
 CRGB leds[NUM_LEDS];
+
 int base[WIDTH];
 int ons[WIDTH];
-
-CRGB Red = CRGB(255,0,0);
-CRGB Green = CRGB(0,255,0);
-CRGB Blue = CRGB(0,0,255);
-CRGB Yellow = CRGB(255,255,0);
-CRGB Purple = CRGB(178, 0, 255);
-CRGB Lightblue = CRGB(0, 255, 255);
-CRGB Pink = CRGB( 255, 43, 148);
-CRGB Orange = CRGB( 255, 153, 0);
-
-CRGB colors[100] = {Red, Orange, Yellow, Purple, Lightblue, Pink, Blue, Green};
-
+const int stay_b = 5; // change bases
+int change_b = 0;
+int k = 0; // rainbow color bias
+const int stay_k = 3;  // last more on a color
+int change_k = 0;
 
 void setup() {
   FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);
@@ -48,13 +42,26 @@ void loop() {
     }
 
   for (int i = 0; i < WIDTH; i++){
-    fade_to_white(i, colors[i], ons[i]);
+    fade_to_white(i, Scroll((i+k)*15 % 256), ons[i]);
   }
   
   for(int i = 0; i < WIDTH; i++){
     ons[i] = base[i];
     }
 
+  change_k++;
+  if (change_k == stay_k){
+      k = (k+1) % 256;
+      change_k = 0;
+    }
+
+  change_b++;
+  if (change_b == stay_b){
+    for(int i = 0; i < WIDTH; i++){
+      ons[i] = base[i] = random(6, 12);
+      }
+      change_b = 0;
+    }
 }
 
 
@@ -76,3 +83,23 @@ void fade_to_white(int col_idx, CRGB curr, int len){
     }
     FastLED.show();
   }
+
+
+// rainbow colors
+CRGB Scroll(int pos) {
+  CRGB color (0,0,0);
+  if(pos < 85) {
+    color.g = 0;
+    color.r = ((float)pos / 85.0f) * 255.0f;
+    color.b = 255 - color.r;
+  } else if(pos < 170) {
+    color.g = ((float)(pos - 85) / 85.0f) * 255.0f;
+    color.r = 255 - color.g;
+    color.b = 0;
+  } else if(pos < 256) {
+    color.b = ((float)(pos - 170) / 85.0f) * 255.0f;
+    color.g = 255 - color.b;
+    color.r = 1;
+  }
+  return color;
+}
